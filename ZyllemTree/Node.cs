2dba5a9d0f37499969b30bc4a236e318194e98eb
IsuruKurulugama <isurukurulugama@ymail.com>
2019-12-15 15:34:24 +0800

    Initial check-in of module ZyllemTree

---
+++ ZyllemTree/Node.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZyllemTree
+{
+    public class Node
+    {
+        public List<object> ObjList { get; set; }
+        public List<Node> BranchList { get; set; }
+
+        public Node() { }
+
+        public Node(List<object> objList, List<Node> branchList)
+        {
+            this.ObjList = objList;
+            this.BranchList = branchList;
+        }
+
+        public int CountItemsByName(Node treeNode, string itemName)
+        {
+            int count = 0;
+            if (treeNode.ObjList != null)
+            {
+                foreach (var node in treeNode.ObjList)
+                {
+                    if (node.ToString().ToLower() == itemName.ToLower()) count++;
+                }
+            }
+
+            if (treeNode.BranchList != null)
+            {
+                foreach (var pred in treeNode.BranchList)
+                {
+                    count += CountItemsByName(pred, itemName);
+                }
+            }
+
+            return count;
+        }
+
+        public void CountMostItems(Node tree, List<Counter> countList, string startingBranchName = "0")
+        {
+            var count = tree.ObjList != null ? tree.ObjList.Count : 0;
+            var counter = 0;
+            countList.Add(new Counter() { Branch = $"{startingBranchName}", Count = count });
+
+            if (tree.BranchList != null)
+            {
+                foreach (var branch in tree.BranchList)
+                {
+                    counter++;
+                    CountMostItems(branch, countList, $"{startingBranchName}.{counter}");
+                }
+            }
+        }
+
+        public void ContainOnlyOneItem(Node tree, List<Counter> countList, string itemName, string startingBranchName = "0")
+        {
+            var count = tree.ObjList != null ? tree.ObjList.Count : 0;
+            var counter = 0;
+
+            if (tree.ObjList != null)
+            {
+                var itemCount = tree.ObjList.GroupBy(x => x).Select(y => y.Key);
+                if (itemCount.ToList().Count == 1 && itemCount.Contains(itemName))
+                    countList.Add(new Counter() { Branch = $"{startingBranchName}", Count = count });
+            }
+
+            if (tree.BranchList != null)
+            {
+                foreach (var branch in tree.BranchList)
+                {
+                    counter++;
+                    ContainOnlyOneItem(branch, countList, itemName, $"{startingBranchName}.{counter}");
+                }
+            }
+        }
+    }
+
+}
+
+
+public class Counter
+{
+    public string Branch { get; set; }
+    public int Count { get; set; }
+}
---
+++ ZyllemTree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ZyllemTree
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                string fruitName = "";
+                int quesNumber = 0;
+                var tree = new Node(null, new List<Node>() {
+                                new Node(null, new List<Node>()),
+                                new Node(new List<object>(){ "Apple" }, null),
+                                new Node(null, new List<Node>()),
+                                new Node(new List<object>(){ "Orange", "Orange", "Orange" }, null),
+                                new Node(null, new List<Node>(){
+                                    new Node(new List<object>() { "Apple", "Orange" }, null),
+                                    new Node(null, new List<Node>() {
+                                        new Node(new List<object>() { "Apple" }, null),
+                                        new Node(new List<object>() { "Orange", "Orange", "Apple" }, null)
+                                    }),
+                                    new Node(new List<object>() { "Orange" }, null),
+                                    new Node(new List<object>() { "Apple", "Apple" }, null)
+                                })
+                                        });
+                ShowQuestions();
+                quesNumber = Convert.ToInt32(Console.ReadLine());
+
+                switch (quesNumber)
+                {
+                    case 1:
+                        Console.WriteLine("Enter fruit name: ");
+                        fruitName = Console.ReadLine();
+                        var fruitCount = tree.CountItemsByName(tree, fruitName);
+                        Console.WriteLine(fruitName + " Count is: " + fruitCount);
+                        break;
+                    case 2:
+                        var counter = new List<Counter>();
+                        tree.CountMostItems(tree, counter);
+                        var maxCount = counter.Max(x => x.Count);
+                        var maxBranch = string.Join(", ", counter.Where(x => x.Count == maxCount).Select(z=>z.Branch).ToList());
+                        Console.WriteLine(maxBranch + " branches has max fruits");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter fruit name: ");
+                        fruitName = Console.ReadLine();
+                        var counterCon = new List<Counter>();
+                        tree.ContainOnlyOneItem(tree, counterCon, fruitName);
+                        var branches = string.Join(", ", counterCon.Select(x => x.Branch).ToList());
+                        Console.WriteLine(branches + " branches has " + fruitName);
+                    break;
+                default:
+                    break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+
+        private static void ShowQuestions()
+        {
+            Console.WriteLine("1. Find fruits count");
+            Console.WriteLine("2. Which branch has most number of fruits");
+            Console.WriteLine("3. Branches by fruit name");
+            Console.WriteLine("Enter Question number: ");
+        }
+    }
+}
